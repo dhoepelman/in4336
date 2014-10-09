1255a71a7590c5365a2db66191f6bd3ee3551c4e
David Hoepelman <dhoepelman@gmail.com>
2014-10-09 16:15:25 +0200

    Delete translated file

--- gc/benchmark.py
+++ gc/benchmark.py
@@ -71,39 +71,44 @@ try:
 
         id = "gc-%s-%d" % (instancename, guess)
         translationfn = "%s/%s.cnf" % (translationdir, id)
-        gc_string_to_sat_file(instance, translationfn, guess)
+        try:
+            gc_string_to_sat_file(instance, translationfn, guess)
 
-        time_this_translation = time.clock() - starttime
-        trace[guess]['trans'] = time_this_translation
+            time_this_translation = time.clock() - starttime
+            trace[guess]['trans'] = time_this_translation
 
-        print("Translation complete, now starting to solve")
+            print("Translation complete, now starting to solve")
 
-        starttime = time.clock()
+            starttime = time.clock()
 
-        with open("%s/%s.cnf" % (solutiondir, id), 'wb') as solutionf:
-            solverresult = subprocess.call("lingeling " + translationfn, shell=True, stdout=solutionf)
+            with open("%s/%s.cnf" % (solutiondir, id), 'wb') as solutionf:
+                solverresult = subprocess.call("lingeling " + translationfn, shell=True, stdout=solutionf)
 
-        # We're a bit screwed if the alarm signal happens exactly here before the next loop iteration, but what are the odds?
-        time_this_solving = time.clock() - starttime
-        time_spent_solving +=  time_this_solving
-        time_spent_translating += time_this_translation
-        trace[guess]['solve'] = time_this_solving
-        trace[guess]['this'] = time_this_translation+time_this_solving
-        trace[guess]['total'] = time_spent_solving+time_spent_translating
+            # We're a bit screwed if the alarm signal happens exactly here before the next loop iteration, but what are the odds?
+            time_this_solving = time.clock() - starttime
+            time_spent_solving +=  time_this_solving
+            time_spent_translating += time_this_translation
+            trace[guess]['solve'] = time_this_solving
+            trace[guess]['this'] = time_this_translation+time_this_solving
+            trace[guess]['total'] = time_spent_solving+time_spent_translating
 
-        if solverresult == 10:
-            # Satisfiable
-            upper_bound = guess
-        elif solverresult == 20:
-            # Unsatisfiable
-            lower_bound = guess
+            if solverresult == 10:
+                # Satisfiable
+                upper_bound = guess
+            elif solverresult == 20:
+                # Unsatisfiable
+                lower_bound = guess
 
-        if lower_bound == upper_bound-1:
-            # We've found the smallest k! It's the upper bound
-            solution = upper_bound
-            print("Found solution: %d" % solution)
-        else:
-            print("New bounds: (%d,%.0f guess was %d" % (lower_bound, upper_bound, guess))
+            if lower_bound == upper_bound-1:
+                # We've found the smallest k! It's the upper bound
+                solution = upper_bound
+                print("Found solution: %d" % solution)
+            else:
+                print("New bounds: (%d,%.0f guess was %d" % (lower_bound, upper_bound, guess))
+        finally:
+            # Delete the translation file, since it can become several gigs
+            if os.path.isfile(translationfn):
+                os.remove(translationfn)
 except KeyboardInterrupt:
     # User wants to cancel
     pass
